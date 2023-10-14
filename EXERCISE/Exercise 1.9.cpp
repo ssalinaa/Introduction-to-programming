7cb58f2c4a8e2164b1f0c89dbe7628a87e30470c
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2023-10-14 15:26:53 +0300

    Add files via upload

---
+++ EXERCISE/Exercise 1.9.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+/*
+1.9. Problem 2.40.[1] To write a program that (via a for loop) finds
+the sum of every third integer starting at 2 and not exceeding n
+(ie the sum 2 + 5 + 8 + 11 + ...)
+*/
+
+int main() {
+
+    int n;
+    std::cout << "Enter an integer n: ";
+    std::cin >> n;
+
+    int sum = 0;
+
+    for (int i = 2; i <= n; i += 3) {
+        sum += i;
+    }
+
+    std::cout << "The sum of every third integer from 2 to " << n << " is: " << sum << std::endl;
+
+    return 0;
+}
