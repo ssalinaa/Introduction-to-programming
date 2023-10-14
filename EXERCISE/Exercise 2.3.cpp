312c1b5a49be0718e9b6d1a41516f14630f88b02
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2023-10-14 17:13:23 +0300

    Add files via upload

---
+++ EXERCISE/Exercise 2.3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+/*
+2.3. Task 2.57.[1] Write a Boolean function that checks whether the sum of the digits of a parameter is a positive integer
+multiple of 3.
+*/
+
+bool isMultipleOf3(int number) {
+
+	int sum = 0;
+	while (number > 0) {
+
+		sum += number % 10;
+		number /= 10;
+	}
+	return sum % 3 == 0;
+}
+
+int main() {
+
+	int number;
+	std::cin >> number;
+
+	std::cout << std::boolalpha << isMultipleOf3(number) << std::endl;
+
+	return 0;
+}
