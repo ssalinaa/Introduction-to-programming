fe58aec8e06e957e206f6263608e5bc43028c745
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2023-10-14 15:20:58 +0300

    Add files via upload

---
+++ EXERCISE/Exercise 1.11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+/*
+1.11. For keyboard-entered natural numbers n and k, to check and
+print on the screen whether n is an exact power of the number k.
+*/
+
+int main() {
+
+    int n, k;
+    std::cin >> n >> k;
+
+    int temp = n;
+    int power = 0;
+
+    for (power = 0; temp % k == 0; power++) {
+        temp /= k;
+    }
+
+    if (temp == 1) {
+        std::cout << n << " is an exact power of " << k << ". " << k << "^" << power << " = " << n << std::endl;
+    }
+    else {
+        std::cout << n << " is not an exact power of " << k << "." << std::endl;
+    }
+
+    return 0;
+}
