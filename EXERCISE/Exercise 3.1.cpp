066ab7cf4aa94f5b18d5b18a93a073b1dcc6c6a3
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2023-10-14 17:36:38 +0300

    Add files via upload

---
+++ EXERCISE/Exercise 3.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <climits>
+
+/*
+3.1. Task 3.1. [1] Write a program that inputs an array of n targets
+numbers (1 ≤ n ≤ 50) and finds and outputs the minimum of them.
+*/
+
+int main() {
+
+    int n;
+    std::cout << "Enter the number of integers (n): ";
+    std::cin >> n;
+
+    if (n < 1 || n > 50) {
+
+        std::cout << "Invalid number of integers. The count should be between 1 and 50." << std::endl;
+        return 1; // Exit with an error
+    }
+
+    int minNumber = INT_MAX; // Initialize the variable with the maximum value
+
+    for (size_t i = 0; i < n; i++)
+    {
+        int number;
+        std::cin >> number;
+
+        if (number < minNumber) {
+
+            minNumber = number;
+        }
+    }
+
+    std::cout << "The minimum number in the sequence is: " << minNumber << std::endl;
+
+    return 0;
+}
+
+
