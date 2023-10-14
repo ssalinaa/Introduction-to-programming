2dddb1f2208f5bacf01c9cc4025d37a5fe9811c4
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2023-10-14 15:34:53 +0300

    Add files via upload

---
+++ EXERCISE/Exercise 1.7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+/*
+1.7. Problem 2.12.[1] Write a program that checks if a given year is a leap year.
+*/
+
+int main() {
+
+    int year;
+    std::cin >> year;
+
+    bool isLeapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    std::cout << std::boolalpha << isLeapYear << std::endl;
+
+    return 0;
+}
